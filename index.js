289dde1c2e952db566302dab3aa2bab97cf5ec2b
Brett Neese <brett@neese.rocks>
2017-12-08 18:17:13 -0800

    add some notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const _each = require('lodash.foreach');
 const awsParamStore = require('aws-param-store');
+
+// declare a global results object
 var results = {};
 
 function getBasePath(path) {
@@ -10,20 +12,34 @@ function getBasePath(path) {
 }
 
 module.exports = function(path) {
+  // get the last part of the path
   let key = path.split('/').pop();
-  let r = {};
+  
+  // get the basePath (everything except the key) from the SSM path
+  // ie /test/staging/FOO's basePath is just /test/staging
+  // this way we can have multiple path patterns in our config.js. 
+  // although idk why you'd actually want to do that.
+  
   let basePath = getBasePath(path);
+  
+  // declare a local results object
+  let r = {};
 
+  // if we don't already have results globally from this particular path, go ahead and query AWS
   if (!results[basePath]) {
-
+    // do this sync because we need variables before app bootup anyway
     let parameters = awsParamStore.newQuery(basePath).executeSync();
+
+    // filter through the results and give them a nice key/value structure
     _each(parameters, function(requestResult) {
       let k = requestResult.Name.split('/').pop();
       r[k] = requestResult.Value;
     });
-
+   
+    // add the results from the query to the global results object keyed under their basepath
     results[basePath] = r;
   }
 
+  // return the results for this particular basePath/key pair
   return results[basePath][key];
 };
